915489491d760c03f3ceab4258221632c8f94ecb
pedro-donoso <pedro.donoso.r@usach.cl>
2024-06-04 02:23:56 -0400

    agrego numeros

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingresa el primer número: ");
+        int a = sc.nextInt();
+        System.out.print("Ingresa el segundo número: ");
+        int b = sc.nextInt();
+        System.out.print("Ingresa el tercer número: ");
+        int c = sc.nextInt();
+        if (a >= b && a >= c) {
+            System.out.printf("El primer número (%d) es el mayor.%n", a);
+        } else if (b >= c) {
+            System.out.printf("El segundo número (%d) es el mayor.%n", b);
+        } else {
+            System.out.printf("El tercer número (%d) es el mayor.%n", c);
+        }
+        sc.close();
     }
 }
